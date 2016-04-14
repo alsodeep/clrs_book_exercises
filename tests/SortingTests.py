765c86e466098de1289397b34629db5699a9ba7d
Sergey Lysenko <alsodeep@speaktoit.com>
2016-04-14 14:08:57 -0700

    add setUp method, remove duplicate code.

--- tests/SortingTests.py
+++ tests/SortingTests.py
@@ -6,15 +6,15 @@ from sorting.InsertionSort import *
 
 
 class SortingTests(unittest.TestCase):
+    def setUp(self):
+        self.a = [31, 41, 59, 26, 41, 58]
+        self.b = [1, 0, 1]
+
     def test_insertion_sorting(self):
-        a = [31, 41, 59, 26, 41, 58]
-        self.assertEqual(insertion_sort(a), [26, 31, 41, 41, 58, 59])
+        self.assertEqual(insertion_sort(self.a), [26, 31, 41, 41, 58, 59])
 
     def test_reverse_insertion_sorting(self):
-        a = [31, 41, 59, 26, 41, 58]
-        self.assertEqual(insertion_sort_reverse(a), [59, 58, 41, 41, 31, 26])
+        self.assertEqual(insertion_sort_reverse(self.a), [59, 58, 41, 41, 31, 26])
 
     def test_binary_addition(self):
-        a = [1, 0, 1]
-        b = [1, 0, 1]
-        self.assertEqual(binary_addition(a, b), [0, 1, 0, 1])
+        self.assertEqual(binary_addition(self.b, self.b), [0, 1, 0, 1])
